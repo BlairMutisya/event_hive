826d4b8030a812701c52cb14b9b77d4eff198b83
Robert Gathanua <robertgathanua@gamail.com>
2024-07-09 16:46:13 +0300

    Handle submit constant

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -8,6 +8,33 @@ const Contact = () => {
     subject: Yup.string().required("Subject is required"),
     message: Yup.string().required("Message is required"),
   });
+  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
+    try {
+      const response = await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Origin: "http://localhost:3000/contact", // Specify the origin of the request
+          // Add any other headers needed for your request
+        },
+        body: JSON.stringify(values),
+        credentials: "same-origin", // Include credentials like cookies if necessary
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      alert("Email sent successfully!");
+      resetForm(); // Clear the form fields
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Failed to send email.");
+    } finally {
+      setSubmitting(false);
+    }
+  };
 }
 
 export default Contact
