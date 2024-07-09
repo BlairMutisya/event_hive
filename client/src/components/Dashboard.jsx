984245e437a80cfba59bdeed07c5cfdbb5afc7bd
Robert Gathanua <robertgathanua@gamail.com>
2024-07-09 16:54:51 +0300

    Create the Dasboard section

---
+++ client/src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventForm from './EventForm'; // Adjust path as needed
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <EventForm />
+    </div>
+  );
+}
+
+export default Dashboard;
