ccff54972fc3f8f77f136b0c654058b0cddd0cbb
BlairMutisya <tonnyblair09@gmail.com>
2024-07-10 10:15:23 +0300

    Import the necessary imports

--- server/app.py
+++ server/app.py
@@ -2,9 +2,19 @@
 
 # Standard library imports
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask import Flask, request, jsonify, Response
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity
+import random
+
 
 # Local imports
 from config import app, db, api
