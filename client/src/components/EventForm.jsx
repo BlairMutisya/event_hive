b953209f038832ed3ba4c41722d21cccaa8829c3
zawadinjagi <zawadinjagi619@gmail.com>
2024-07-10 12:12:56 +0300

    adding validations to the event form file

--- client/src/components/EventForm.jsx
+++ client/src/components/EventForm.jsx
@@ -19,3 +19,21 @@ const EventForm = () => {
     imageUrl: "", 
   };
   
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("Title is required"),
+    description: Yup.string().required("Description is required"),
+    date: Yup.date().required("Date is required"),
+    location: Yup.string().required("Location is required"),
+    medium: Yup.string().required("Medium is required"),
+    startDate: Yup.date().required("Start Date is required"),
+    endDate: Yup.date().required("End Date is required"),
+    startTime: Yup.string().required("Start Time is required"),
+    endTime: Yup.string().required("End Time is required"),
+    maxParticipants: Yup.number().required("Max Participants is required"),
+    category: Yup.string().required("Category is required"),
+    acceptReservation: Yup.boolean().required("Accept Reservation is required"),
+    imageUrl: Yup.string()
+      .url("Must be a valid URL")
+      .required("Image URL is required"),
+  });
+  
